feb2df10d0e680323a5b7532d566ef9c7cac45ce
josefdzbeta <jafb19@outlook.com>
2022-02-20 22:33:47 +0100

    control

--- formulario-angular/src/app/hero-form/hero-form.component.ts
+++ formulario-angular/src/app/hero-form/hero-form.component.ts
@@ -7,9 +7,18 @@ import { Hero } from "../hero";
   styleUrls: ['./hero-form.component.css']
 })
 export class HeroFormComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
   powers = [ 'Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
 
-  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck OverStreet')
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck OverStreet');
+
+  submitted = false;
+
+  onSubmit(){
+    this.submitted = true
+  }
 
 }
